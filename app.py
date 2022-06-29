4e1491fc548147497cf9f2e9319ebe1e6fcb9e6c
Branley Minaya <bminaya23@gmail.com>
2022-06-28 22:26:05 -0400

    classess

--- app.py
+++ app.py
@@ -1,30 +1,18 @@
-from timeit import timeit
+class Point:
+    default_color = "red"
 
-code1 = """
-def calculate_xfactor(age):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-    if age <= 0:
-        raise ValueError("age cannot be 0 of less")
-    return 10 / age
-
-
-try:
-    calculate_xfactor(-2)
-except ValueError as error:
-    pass
-"""
-
-code2 = """
-def calculate_xfactor(age):
-
-    if age <= 0:
-        return None
-    return 10 / age
+    def draw(self):
+        print(f"Point ({self.x}, {self.y}")
 
 
-xfactor = calculate_xfactor(-1)
-if xfactor == None:
-  pass
-"""
-print("first code=", timeit(code1, number=100000))
-print("second code=", timeit(code2, number=100000))
+point = Point(1, 2)
+print(point.default_color)
+print(Point.default_color)
+point.draw()
+
+another = Point(3, 4)
+another.draw()
