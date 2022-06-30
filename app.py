e0ed07868b3f7f99d288fb0f7f76c9c535b7da58
Branley Minaya <bminaya23@gmail.com>
2022-06-29 20:37:10 -0400

    operation

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
 class Point:
-    default_color = "red"
-
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    def draw(self):
-        print(f"Point ({self.x}, {self.y}")
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
 
 
 point = Point(1, 2)
-print(point.default_color)
-print(Point.default_color)
-point.draw()
-
-another = Point(3, 4)
-another.draw()
+other = Point(1, 2)
+print(point == other)
