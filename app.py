380ee4914b47faec9bd29479933faeb5e3685922
Branley Minaya <bminaya23@gmail.com>
2022-07-02 07:43:18 -0400

    for loop

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-class Employee:
-    def greet(self):
-        print("Employee Greet")
+# print all number from 1 to 10
 
+# for x in range(1, 101):
+#     for y in range(1, 11):
+#         if x * y % 2 == 0:
+#             print(x * y)
 
-class Person:
-    def greet(self):
-        print("Person Greet")
+#     print("moving on to next x value")
 
+# print("done")
 
-class Manager(Employee, Person):
-    pass
+names = ["bran", "ana", "joe", "nate"]
+
+for name in names:
+    print(name)
+# names is an iratable. Can get indiviual items. every item in names Im going to refer to as name and do something with it inside this for loop
