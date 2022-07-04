eceb14efc1448ff88d0496aaab6b9b3d1efaf525
Branley Minaya <bminaya23@gmail.com>
2022-07-04 12:26:24 -0400

    suits and values

---
+++ deck_cards/app.py
@@ -0,0 +1,7 @@
+# 1) creating a stack or deck of cards
+class Deck():
+
+    def __init__(self):
+
+        self.suits = ['hearts', 'spades', 'diamonds', 'clubs']
+        self.values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
