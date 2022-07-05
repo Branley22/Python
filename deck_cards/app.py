8cadad3215251edc10bdbfead0e5421e2ed77b54
Branley Minaya <bminaya23@gmail.com>
2022-07-04 21:40:40 -0400

    flask

--- deck_cards/app.py
+++ deck_cards/app.py
@@ -45,15 +45,34 @@ class Deck():
         #         self.contents.append(Card(suit, value))
 
         self.contents = [Card(suit, value)
-                         for suit in self.suits for calue in values]
+                         for suit in self.suits for value in self.values]
+
+        self.shuffle_deck()
 
         # 7) swap deck of cards
+    def shuffle_deck(self):
         for i in range(0, len(self.contents)):
             x = randint(0, len(self.contents) - 1)
             self.contents[i], self.contents[x] = self.contents[x], self.contents[i]
 
+    def deal_card(self):
+        if len(self.contents) == 0:
+            return None
+        return self.contents.pop()
+
+    def deal_multiple_cards(self, number_of_cards):
+        cards = []
+        for i in range(0, number_of_cards):
+            cards.append(self.deal_card())
+        return cards
+
 
 new_deck = Deck()
 
+# 8) deal
+player_1 = []
+player_2 = []
+
+
 print(new_deck.contents)
-print(new_deck.contents[0])
+player_1.append(new_deck.deal_card())
